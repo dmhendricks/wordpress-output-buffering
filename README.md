53e50ed5d45c452b1bd218f9070ba1455737a2a3
Daniel Hendricks <mail@danhendricks.com>
2017-07-19 13:49:03 -0500

    Disabled if is_admin()

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 A simple [mu-plugin](https://codex.wordpress.org/Must_Use_Plugins) that buffers the entire WP process, capturing the final output for manipulation.
 
 [Original code](http://stackoverflow.com/a/22818089/3799374) by [kfriend](https://stackoverflow.com/users/419673/kfriend) on Stack Overflow. Licensed as GPL because it is a WordPress derivative.
+
+## Changelog
+
+**1.0.2 (master)**
+* Disabled in WP Admin and when DOING_AJAX
+
+**1.0.0**
+* Initial commit
--- output-buffering.php
+++ output-buffering.php
@@ -1,25 +1,36 @@
 <?php
 /**
- * Output Buffering
- *
- * Buffers the entire WP process, capturing the final output for manipulation.
+ * @wordpress-plugin
+ * Plugin Name:       Output Buffering
+ * Plugin URI:        https://github.com/dmhendricks/wordpress-output-buffering
+ * Description:       Buffers the entire WP process, capturing the final output for manipulation.
+ * Version:           1.0.2
+ * Author:            Daniel M. Hendricks
+ * Author URI:        https://www.danhendricks.com
+ * License:           GPL-2.0
+ * License URI:       https://opensource.org/licenses/GPL-2.0
+ */
+
+/*
+ * Original author: http://stackoverflow.com/a/22818089/3799374
  */
 
 ob_start();
 
-add_action('shutdown', function() {
+if( !is_admin() && !( defined('DOING_AJAX') && DOING_AJAX ) ) {
+  add_action('shutdown', function() {
     $final = '';
 
-    // We'll need to get the number of ob levels we're in, so that we can iterate over each, collecting
+    // We'll need to get the number of OB levels we're in, so that we can iterate over each, collecting
     // that buffer's output into the final output.
     $levels = ob_get_level();
-
     for ($i = 0; $i < $levels; $i++)
     {
-        $final .= ob_get_clean();
+      $final .= ob_get_clean();
     }
 
     // Apply any filters to the final output
     echo apply_filters('final_output', $final);
-}, 0);
+  }, 0);
+}
 ?>
